bb93b971ff2202445f3e83964cd56f883cab655b
eunseon <kwon9348@naver.com>
2024-12-11 23:52:47 +0900

    Thread 생성 예제

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,31 @@
 #include <iostream>
 #include "Corepch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+	cout << "Hello Thread " << endl;
+}
+
+void HelloThread_2(int32 num)
+{
+	cout << num << endl;
+}
+
 int main()
 {
-    HelloWorld();
+	vector<std::thread> v;
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		v.push_back(std::thread(HelloThread_2, i));
+	}
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+
 }
