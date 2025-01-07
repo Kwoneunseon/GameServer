7c1a63d30dfc38f75e6b041d3da278c27da1b559
eunseon <kwon9348@naver.com>
2025-01-07 23:52:02 +0900

    lock_guard, unique_lockguard 예제

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -29,7 +29,13 @@ int main()
 {
 	for (int32 i = 0; i < 10000; i++)
 	{
-		LockGuard<std::mutex> lockGuard(m);
+		//std::lock_guard<std::mutex> lockGuard(m);
+
+		// 당장 lock을 잠그지 않고, 인터페이스만 생성
+		std::unique_lock<std::mutex> uniqueLock(m, std::defer_lock);
+
+		// 잠기는 시점을 설정 가능 -> lock_guard와 차이점.
+		uniqueLock.lock();
 
 		std::cout << i;
 	}
